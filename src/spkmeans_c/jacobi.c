35aa96af9f0613ada0b12080b4c97365f4a6d026
ubuntu <ubuntu@ubuntu-X299-UD4-Pro>
2023-03-08 15:47:48 +0200

    fixed -0.000 jacobi handeling

--- src/spkmeans_c/jacobi.c
+++ src/spkmeans_c/jacobi.c
@@ -149,7 +149,8 @@ double off_diagonal_square(Matrix mat, size_t n) {
 static void _unsign_zero_in_jacobi_result(JacobiResult *jr, size_t n) {
     size_t i, j;
     for (j = 0; j < n; j++) {
-        if (jr -> eigenvalues[i] == 0 && signbit(jr -> eigenvalues[i]) != 0) {
+        if ((jr -> eigenvalues[i] > -0.0001 && jr -> eigenvalues[i] < 0) ||
+            (jr -> eigenvalues[i] == 0 && signbit(jr -> eigenvalues[i]) != 0)) {
             jr -> eigenvalues[i] = 0.0;
             for (i = 0; i < n; i++) {
                jr -> eigenvectors[i][j] = -(jr -> eigenvectors[i][j]);
