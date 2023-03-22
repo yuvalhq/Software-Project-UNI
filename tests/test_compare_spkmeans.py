c0bfa81e5e9525680c26514834210a5c44539e49
ubuntu <ubuntu@ubuntu-X299-UD4-Pro>
2023-03-22 18:24:12 +0200

    fixed merge problem

--- tests/test_compare_spkmeans.py
+++ tests/test_compare_spkmeans.py
@@ -17,10 +17,6 @@ compare_input_output_test = pytest.mark.parametrize(
             str(Path(__file__).parent.joinpath("testfiles").resolve()),
             str(Path(__file__).parent.joinpath("testfiles/ofek_s").resolve()),
         ),
-                (
-            str(Path(__file__).parent.joinpath("testfiles").resolve()),
-            str(Path(__file__).parent.joinpath("testfiles/sophie").resolve()),
-        ),
     ],
 )
 
